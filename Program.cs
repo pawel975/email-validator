f88e7faa340047db8e31d6fee0a825575ac1aceb
Paweł Kurek <pawel.kurek975@gmail.com>
2023-10-08 17:34:29 +0200

    email validation without specyfing mistake

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Text.RegularExpressions;
+
+namespace emailValidator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"^[a-z\d]+\.?[a-z\d]+@{1}[a-z]+\.[a-z]{2,3}$");
+            string userInput;
+
+            do
+            {
+                Console.WriteLine("Type in email to validate \\ 'x' to Escape");
+                Console.Write("Email: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == "x")
+                {
+                    break;
+                }
+
+                if (regex.IsMatch(userInput))
+                {
+                    Console.WriteLine("\nEmail address is correct!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid address...\n");
+                }
+
+            } while (userInput != "x");
+
+
+        }
+
+    }
+}
